f9777effd179dd17fe363cb97cf4b1399d86a58a
qwerty <93528492+CodeAkyra@users.noreply.github.com>
2024-09-25 13:32:51 +0800

    rename

---
+++ modal-bug/src v1/scripts/test_modal1.js
@@ -0,0 +1,14 @@
+document.getElementById('openModalBtn').addEventListener('click', function() {
+    localStorage.setItem('openModal', 'true'); // Set a flag to open modal
+    window.location.href = 'main.html'; // Redirect to the index page
+});
+/* FOR TEST PURPOSES LANG SIYA SA MODAL */
+/* FOR TEST PURPOSES LANG SIYA SA MODAL */
+/* FOR TEST PURPOSES LANG SIYA SA MODAL */
+/* FOR TEST PURPOSES LANG SIYA SA MODAL */
+/* FOR TEST PURPOSES LANG SIYA SA MODAL */
+/* FOR TEST PURPOSES LANG SIYA SA MODAL */
+/* FOR TEST PURPOSES LANG SIYA SA MODAL */
+/* FOR TEST PURPOSES LANG SIYA SA MODAL */
+/* FOR TEST PURPOSES LANG SIYA SA MODAL */
+/* FOR TEST PURPOSES LANG SIYA SA MODAL */
